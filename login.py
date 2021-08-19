9bb76db4b104499a2d65e93e6c14cf9deff4c400
louie0621 <louiecagas1210@gmail.com>
2021-08-19 17:16:22 +0800

    commited

--- login.py
+++ login.py
@@ -7,12 +7,14 @@ screen = f"{screen_width}{'x'}{screen_height}"
 window.geometry(screen)
 window.configure(bg = "#b6c0ff")
 
-photo = PhotoImage(file = 'icon/bg.png')
-Label(window,image='photo')
+bgphoto = PhotoImage(file = 'icon/bg.png')
+bglbl = Label(window,image=bgphoto)
+bglbl.pack()
+bglbl.place(x=0,y=0)
 
 #login
 logincanvas = Canvas(
-    bg = "#9fa6ce",
+    bg = "#46caf2",
     width = 350,
     height = 390,
     bd = 5,
@@ -25,14 +27,14 @@ logincanvas.place(x=500,y=150)
 lbladmin = Label(window,
     text = "Admin",
     fg = "black",
-    bg = "#9fa6ce",
+    bg = "#46caf2",
     font = ('Times',35)
 )
 lbladmin.pack()
 lbladmin.place(x=610,y=160)
 
 #username
-usrlbl = Label(window, text = "Username", font=('Times',25), bg="#9fa6ce", fg="black")
+usrlbl = Label(window, text = "Username", font=('Times',25), bg="#46caf2", fg="black")
 usrlbl.pack()
 logincanvas.create_window(100, 110,window=usrlbl)
 usrentry = Entry(window, font=('Times',20), relief="flat")
@@ -40,21 +42,27 @@ usrentry.pack()
 logincanvas.create_window(180,150, window=usrentry)
 
 #password
-passlbl = Label(window, text="Password", font=('Times',25), bg="#9fa6ce",fg= "black")
+passlbl = Label(window, text="Password", font=('Times',25), bg="#46caf2",fg= "black")
 passlbl.pack()
 logincanvas.create_window(100,200,window=passlbl)
-passentry = Entry(window, font=('Times',20), relief="flat")
+passentry = Entry(window, font=('Times',20), relief="flat", show="*")
 passentry.pack(ipady=3)
 logincanvas.create_window(180,240,window=passentry)
 
 
 #login btn
 login_btn = PhotoImage(file='icon/loginkey.png')
-submit_btn = Button(window, image=login_btn, borderwidth=0 , bg="#9fa6ce", activebackground = "#9fa6ce", relief="groove")
+submit_btn = Button(window,
+                    image=login_btn,
+                    borderwidth=0 ,
+                    bg="#46caf2",
+                    activebackground = "#46caf2",
+                    relief="groove",
+                    cursor = "arrow")
 submit_btn.pack()
 logincanvas.create_window(190,325, window=submit_btn)
 
 
-window.config(cursor="")
-window.overrideredirect(1)
+#window.config(cursor="none")
+#window.overrideredirect(1)
 window.mainloop()
